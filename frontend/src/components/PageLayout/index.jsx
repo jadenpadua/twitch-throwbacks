a1f2488335b57bebf65c777c81cf53485cdcd5a3
leestanley <stanleylee91776@gmail.com>
2019-09-28 22:28:01 -0700

    buttons

---
+++ frontend/src/components/PageLayout/index.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import Banner from '../../containers/Banner';
+import NavDropdown from '../../containers/NavDropdown';
+import NavProfile from '../../containers/NavProfile';
+import NavBarHorizontal from '../NavBarHorizontal';
+import NavBarVertical from '../NavBarVertical';
+
+import logo from '../../assets/graphics/logo.svg';
+
+import './style.less';
+
+const PageLayout = props => {
+  return (
+    <>
+      <div className="header">
+        <NavLink className="title" to="/">
+          <img alt="ACM" id="logo" src={logo} />
+          <span className="heading">ACM@UCSD</span>
+          <span className={props.isMobile ? 'hidden' : 'subheading'}>&nbsp;Membership Portal</span>
+        </NavLink>
+        <div className="profile">
+          <NavProfile menu={<NavDropdown />} />
+        </div>
+      </div>
+      <Banner />
+      {props.isMobile ? (
+        <>
+          <NavBarHorizontal />
+          <div className="content">{props.children}</div>
+        </>
+      ) : (
+        <div className="content-table">
+          <NavBarVertical isAdmin={props.isAdmin} />
+          <div className="content">{props.children}</div>
+        </div>
+      )}
+    </>
+  );
+};
+
+const mapSizesToProps = ({ width }) => ({
+  isMobile: width < 768,
+});
+
+PageLayout.propTypes = {
+  isAdmin: PropTypes.bool.isRequired,
+};
+
+export default withSizes(mapSizesToProps)(PageLayout);
